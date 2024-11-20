c8f2d71bfc5fbea510d95f976e797797ba096f9e
yongjunhong <kevin0928@naver.com>
2024-11-20 23:34:30 +0900

    init

---
+++ src/test/java/Technical_Assignment/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Technical_Assignment.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
