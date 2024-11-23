f73471fdaa1e281606d9f2ce5a6dc195375c61e8
Yongjun Hong <kevin0928@naver.com>
2024-11-23 23:01:56 +0900

    유저 및 유저 기록 관리 구조 설계 및 데이터베이스 설정 (#2)
    * feat : sql 설정
    
    * feat : User, UserHistory 엔티티 추가
    
    * feat : docker-compose 파일 추가
    
    - docker-compose를 통해 mariadb 사용

---
+++ src/main/java/Technical_Assignment/demo/user_history/entity/UserHistory.java
@@ -0,0 +1,45 @@
+package Technical_Assignment.demo.user_history.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "USER_HISTORY")
+public class UserHistory {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "history_idx")
+	private Integer id;
+
+	@Column(name = "url", nullable = false, columnDefinition = "TEXT")
+	private String url;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "action_type", nullable = false, columnDefinition = "ENUM('C','U','D')")
+	private ActionType actionType;
+
+	@Column(name = "reg_user_idx", nullable = false)
+	private Integer regUserId;
+
+	@Column(name = "reg_ip", nullable = false, length = 16)
+	private String regIp;
+
+	@Column(name = "reg_dt", nullable = false)
+	private LocalDateTime regDate;
+}
