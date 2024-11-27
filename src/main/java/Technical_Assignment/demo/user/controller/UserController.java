cb97a0af3fb01109f685e4384fb988eed2bdd688
Yongjun Hong <kevin0928@naver.com>
2024-11-27 23:50:00 +0900

    feat : 회원 삭제 구현 (#12)

--- src/main/java/Technical_Assignment/demo/user/controller/UserController.java
+++ src/main/java/Technical_Assignment/demo/user/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -69,5 +70,19 @@ public class UserController {
 	public ResponseEntity<UserUpdateDto> updateUser(@RequestParam String id, @RequestParam String updateName) {
 		UserUpdateDto userUpdate = userService.updateUser(id, updateName);
 		return ResponseEntity.ok(userUpdate);
+	}
+
+	@PreAuthorize("hasRole('ROLE_SYSTEM_ADMIN')")
+	@PostMapping("/delete")
+	public String userDelete(@RequestParam String id) {
+		userService.deleteUser(id);
+		return "redirect:/user/list";
+	}
+
+	@DeleteMapping("/delete")
+	@ResponseBody
+	public ResponseEntity<String> deleteUser(@RequestParam String id) {
+		userService.deleteUser(id);
+		return ResponseEntity.ok("success");
 	}
 }
